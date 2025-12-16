16c0104c7814a94bfada24119778c10c79361689
paingsoeko <dev.kopaing@gmail.com>
2025-12-16 17:40:37 +0700

    feat: support profile

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -36,6 +36,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile()
             ->databaseNotifications()
             ->sidebarCollapsibleOnDesktop()
             ->colors([
